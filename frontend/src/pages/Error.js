7ee88beb433f760d5483ddd7980235ef1b0e9d81
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-19 17:22:50 +0300

    369. Error Handling with Custom Errors

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,4 @@
+function ErrorPage() {
+    return <h1>An Error Occurred!</h1>;
+}
+export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,9 +3,9 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
     const data = useLoaderData();
-    if (data.isError) {
-        return <p>{data.message}</p>;
-    }
+    // if (data.isError) {
+    //     return <p>{data.message}</p>;
+    // }
     const events = data.events;
     return <EventsList events={events} />;
 }
@@ -15,7 +15,8 @@ export default EventsPage;
 export async function loader() {
     const response = await fetch('http://localhost:8080/eventsss');
     if (!response.ok) {
-        return { isError: true, message: 'Could not fetch events.' };
+        // return { isError: true, message: 'Could not fetch events.' };
+        throw { message: 'Could not fetch events.' };
     } else {
         return response;
     }
