9445cca35633260da4ed5b24dbd35c10474873d1
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-19 17:17:42 +0300

    367,1. Example of error handling..

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,6 +3,9 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
     const data = useLoaderData();
+    if (data.isError) {
+        return <p>{data.message}</p>;
+    }
     const events = data.events;
     return <EventsList events={events} />;
 }
@@ -10,9 +13,9 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-    const response = await fetch('http://localhost:8080/events');
+    const response = await fetch('http://localhost:8080/eventsss');
     if (!response.ok) {
-        // ...
+        return { isError: true, message: 'Could not fetch events.' };
     } else {
         return response;
     }
