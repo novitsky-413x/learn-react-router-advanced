e2e75f510b73ce13d1b31bec22fb848d831e46e5
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-23 16:55:57 +0400

    375. Working with action() Functions

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,4 +1,30 @@
+import { redirect } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+
 function NewEventPage() {
-    return <h1>NewEventPage</h1>;
+    return <EventForm />;
 }
 export default NewEventPage;
+
+export async function action({ request, params }) {
+    const formData = await request.formData();
+    const eventData = {
+        title: formData.get('title'),
+        image: formData.get('image'),
+        date: formData.get('date'),
+        description: formData.get('description'),
+    };
+    const response = await fetch('http://localhost:8080/events', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(eventData),
+    });
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not save new event.' }), {
+            status: 500,
+        });
+    }
+    return redirect('/events');
+}
